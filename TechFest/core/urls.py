0e88fc3718b536f36cd12f72fd91cd4126d3c5c2
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2023-01-15 18:21:36 +0800

    .

--- TechFest/core/urls.py
+++ TechFest/core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile', views.profile, name='profile'),
+    path('product', views.product, name='product'),
 ]
--- TechFest/core/views.py
+++ TechFest/core/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def product(request):
+    return render(request, 'product.html')
